fbddb31f628315cf1d779363578a390ba889ed81
MasinVEVO <90444747+MasinVEVO@users.noreply.github.com>
2025-02-11 16:03:15 +0100

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
