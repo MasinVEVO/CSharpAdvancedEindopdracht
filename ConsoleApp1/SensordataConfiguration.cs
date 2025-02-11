d03a1123fd929b44c537392018137e26391c71ed
MasinVEVO <90444747+MasinVEVO@users.noreply.github.com>
2025-02-11 16:30:09 +0100

    singleton fortnite

---
+++ ConsoleApp1/SensordataConfiguration.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApplication;
+
+public sealed class SensordataConfiguration
+{
+    private static SensordataConfiguration _instance;
+    private SensordataConfiguration() { }
+    
+    // De reden voor de singleton aanpak is om een configuratiepunt te maken voor de sensor_data.txt file. Dit onderbehoudt het makkelijker en hoeven er niet 
+    // meerdere instanties gebruikt te worden voor deze configuratiepunt.
+
+    public static SensordataConfiguration Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new SensordataConfiguration();
+            }
+            return _instance;
+        }
+    }
+
+    public string ReadSensorData(string filepath)
+    {
+        if (!File.Exists(filepath))
+        {
+            throw new FileNotFoundException("Bestand is niet gevonden", filepath);
+        }
+
+        return File.ReadAllText(filepath);
+    }
+}
