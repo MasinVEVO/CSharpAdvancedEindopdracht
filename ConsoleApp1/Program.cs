04f68d856436510a99cd3a0b5832e39067ecf393
MasinVEVO <90444747+MasinVEVO@users.noreply.github.com>
2025-02-22 12:41:00 +0100

    directory change

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            string filepath = "sensor_data.txt";
+            string filepath = "C:\\Users\\Gebruiker\\RiderProjects\\Eindopdracht-CSharpAdvanced\\ConsoleApp1\\ConsoleApp1\\sensor_data.txt";
             try
             {
                 string data = SensordataConfiguration.Instance.ReadSensorData(filepath);
